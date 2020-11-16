b66508a6f489bf0bd25b2642532f6a5a32841c72
deepmoss192 <74496068+deepmoss192@users.noreply.github.com>
2020-11-16 21:21:17 +0100

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+##Week 1
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
